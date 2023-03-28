5a27dd9cc7e2189a25645d1223b0e8c5b554085f
Ephrem-shimels21 <eph21sz@gmail.com>
2023-03-28 15:55:21 -0700

    add _printf.c file

---
+++ _printf.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _printf - function for the printf
+ * @format: the format for type specifiers
+ * Return: integer  number of characters printed
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list var;
+}
